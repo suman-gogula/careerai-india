1ed57ff5d0ec35a6a73f6af4d639068df1e7acdd
suman-gogula <sumangogula33@gmail.com>
2026-06-12 15:59:48 +0530

    added pwa icon

--- careerai-india/vite.config.js
+++ careerai-india/vite.config.js
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'CareerAI India',
+        short_name: 'CareerAI',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: '/icon.png',
+            sizes: '192x192',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
